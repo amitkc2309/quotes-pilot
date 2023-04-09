12a6771c5f93a6cf25e706cf7f259dc404a42a5a
achaudhary <amit.chaudhary2@soprabanking.com>
2023-04-09 13:04:30 +0530

    code refactor

--- frontend_web/quote-app/src/index.js
+++ frontend_web/quote-app/src/index.js
@@ -3,25 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import jwt_decode from 'jwt-decode';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-function isAuthenticated(){
-  const token = localStorage.getItem('jwtToken');
-  if (!token) {
-    return false;
-  }
-  try {
-    const { exp } = jwt_decode(token);
-    if (Date.now() < exp * 1000) {
-      return true;
-    }
-  } catch (e) {
-    console.log(e);
-  }
-
-  return true;
-}
 root.render(
   <React.StrictMode>
     <App/>
